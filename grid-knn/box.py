81025c8c63ffe634d0085f5dc305aac20b9205fd
Rayyan Aamir <rayyan22.aamir@gmail.com>
2025-03-21 08:48:05 -0400

    skeleton logic for GridKNN

--- grid-knn/box.py
+++ grid-knn/box.py
@@ -12,24 +12,24 @@ class Box:
     # attr types
     data: list[np.ndarray]
     num_samples: int  # number of data points within this box
-    inner_corner: np.ndarray
+    outer_corner: np.ndarray
     box_size: float
 
-    def __init__(self, data: list[np.ndarray], inner_corner: np.ndarray, box_size: float) -> None:
+    def __init__(self, data: list[np.ndarray], outer_corner: np.ndarray, box_size: float) -> None:
         """
         Box (hypercube) constructor
 
         Parameters:
         - data (list[ndarray]): data points inside this box
-        - inner_corner (ndarray): the corner of this box that is closest (by euclidean distance) to the origin
+        - outer_corner (ndarray): the corner of this box that is furthest (by euclidean distance) from the origin
         """
         self.data = data
         self.num_samples = len(data)
-        self.inner_corner = inner_corner
+        self.outer_corner = outer_corner
         self.box_size = box_size
     
     def __repr__(self) -> str:
         """
         String representation of the box
         """
-        return f"Box(inner_corner={self.inner_corner}, box_size={self.box_size}, num_samples={self.num_samples})"
+        return f"Box(outer_corner={self.outer_corner}, box_size={self.box_size}, num_samples={self.num_samples})"
--- grid-knn/grid_knn.py
+++ grid-knn/grid_knn.py
@@ -15,6 +15,12 @@ class GridKNN:
     """
     kNN model that employs the grid-hashing approach.
     """
+    # attr types
+    k: int  # nearest neighbours to consider
+    n: int  # dimensionality of dataset
+    box_size: float  # size of each box in the partition
+    grid_hashmap: defaultdict[tuple[float], Box]  # stores each of the boxes
+
     def __init__(self, k: int, box_size: float) -> None:
         """
         Initialize the GridKNN model.
@@ -24,8 +30,9 @@ class GridKNN:
         - box_size: size of each box in the grid.
         """
         self.k = k
+        self.n = None  # no dataset yet
         self.box_size = box_size
-        self.grid_hashmap = defaultdict(list)  # stores each of the boxes
+        self.grid_hashmap = defaultdict(list)
 
     def fit(self, data: np.ndarray) -> None:
         """
@@ -35,7 +42,8 @@ class GridKNN:
         - data: ndarray of shape (n_samples, n_features).
         """
         self.data = data
-        self.build_grid()
+        self.n = self.data.ndim
+        self._build_grid()
     
     def fit(self, data: pd.DataFrame) -> None:
         """
@@ -45,36 +53,69 @@ class GridKNN:
         - data: DataFrame of shape (n_samples, n_features).
         """
         self.data = data.values
-        self.build_grid()
+        self.data.ndim
+        self._build_grid()
 
-    def build_grid(self) -> None:
+    def _build_grid(self) -> None:
         """
         Partition the data into a grid and store points in the hashmap.
         """
+        # TODO: get the outermost point (furthest from origin, by euclidean distance)
+        pass
+
+        # TODO: create boxes partitioning the space up to the furthest point, inclusive
+        pass
+
+        # TODO: insert each point into their corresponding box
+        pass
+        
         raise NotImplementedError
 
-    def __get_box(point: np.ndarray) -> Box:
+    def _get_box(self, point: np.ndarray) -> Box:
         """
-        Get the box for a given point.
+        Get the box containing the given point.
         """
-        raise NotImplementedError
+        # TODO: find the interval the point is within for each dimension
+        box_intervals_containing_point = None
+        box_outer_corner = None
+        # get the index for the grid_hashmap (coords of outermost corner)
+        outer_corner_key = tuple(box_outer_corner)
+        # query the grid map to retrieve the box with this outer corner
+        return self.grid_hashmap[outer_corner_key]
 
-    def __get_surrounding_boxes(self, box: Box) -> deque[Box]:
+    def _get_surrounding_boxes(self, boxes: deque[Box]) -> deque[Box]:
         """
         Get a queue representing the boxes surrounding this one.
         """
+        # TODO: get the indices of surrounding boxes' outer corners
+        pass
+        # TODO: query the grid map for the boxes and return in a deque
+        # (the queue is convenient for BFS expansion in _get_k_nearest_neighbours)
+        pass
         raise NotImplementedError
     
-    def __get_k_nearest_neighbours(self, point: np.ndarray) -> list[np.ndarray]:
+    def _get_k_nearest_neighbours(self, point: np.ndarray) -> list[np.ndarray]:
         """
         Get the k nearest neighbours of this point
         """
+        # get the box that the point is within
+        box_containing_point = self._get_box(point)
+        # if it has atleast k points (besides the query point), classify based on them
+        if box_containing_point.num_samples > self.k:
+            # TODO: classify point with the samples in it's own box
+            pass
+        # otherwise, check surrounding boxes until the k nearest neighbours are identified
+        else:
+            # TODO: recurse into the boxes surrounding our current ones
+            boxes = deque([box_containing_point])  # initialize as deque for BFS
+            pass
         raise NotImplementedError
 
-    def __classify_point(self, point: np.ndarray, nearest_neighbours: list[np.ndarray]) -> float:
+    def _mode_class(self, nearest_neighbours: list[np.ndarray]) -> float:
         """
-        Classify a point as the mode of its k nearest neighbours.
+        Get the mode classification of the k nearest neighbours.
         """
+        # TODO: get the mode classification of the nearest neighbours
         raise NotImplementedError
 
     def predict(self, point: np.ndarray) -> float:
@@ -87,8 +128,8 @@ class GridKNN:
         Returns:
         The classification of the point as a float
         """
-        
-        raise NotImplementedError
+        nearest_neighbours = self._get_k_nearest_neighbours(point)
+        return self._mode_class(nearest_neighbours)
 
 # Example usage
 if __name__ == "__main__":
